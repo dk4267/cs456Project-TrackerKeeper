bd12f15b2b0467ae7c101996d06051f3721b0166
cbmunns <cbrockingtonm@gmail.com>
2020-11-21 20:48:15 -0600

    Emotional detail draft added

--- src/screens/mood/EmotionDetailScreen.js
+++ src/screens/mood/EmotionDetailScreen.js
@@ -1,19 +1,215 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, TextInput, ScrollView, TouchableWithoutFeedback, ImageBackground, Image } from 'react-native';
+import { Directions } from 'react-native-gesture-handler';
+
 
 const EmotionDetailScreen = ({ navigation }) => {
+    
+
+    const [mood, setMood] = React.useState('')
+
     return (
-        <View>
-            <Text style={styles.textStyles}>Emotion Detail Screen!</Text>
-            <Button title="Go to journal screen" onPress={() => navigation.navigate('Journal')}/>
+        <View style={styles.container}>
+            
+            <ScrollView>
+                <Text/>
+                <Text style={styles.titleText}>Choose what emotions apply today</Text>
+                <Text/>
+                <View style={styles.flexBox1}>
+
+                    <View style={styles.flexBox2}>
+                        <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/circle.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_dead_14700.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Frazzled</Text>
+                        <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/dark_blue.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_Sad_14699.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Sad</Text>
+                        <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/red_circle.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_angry_14760.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Angry</Text>
+                        <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/pink.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_cry_14757.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Anxious</Text>
+                    </View>
+
+                    <View style={styles.flexBox2}>
+                    <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/green.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_Happy_14703.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Happy</Text>
+                        <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/light_green.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_squint_14705.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Content</Text>
+                        <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/yellow.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_shocked_14702.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Shocked</Text>
+                        <ImageBackground 
+                        style={styles.backroundStyles} 
+                        source={require("./assests/orange.png")}>
+                            <TouchableWithoutFeedback 
+                            onPress={() => navigation.navigate('Journal')}>
+                                <Image 
+                                style={styles.buttonStyles} 
+                                source={require("./assests/noun_Neutral_14701.png")}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
+                        <Text style={styles.textStyles}>Fine</Text>
+                    </View>
+
+
+                    
+                </View>
+                <Text style={styles.titleText}>Write in</Text>
+                <TextInput style={styles.inputStyles} value={mood} multiline={true} maxLength={80} onChangeText={(text) => setMood(text)}/>
+                <View style={styles.flexBox3}><Button
+                  onPress={() => alert("This is a button!")}
+                  title="Save mood"
+                  
+                />
+                <Button style={styles.statsButton} labelStyle={styles.statsButtonText}
+                  onPress={() => alert("This is a button!")}
+                  title="Remove mood"
+                  
+                /></View>
+                <View style={styles.flexBox3}>
+                <Button style={styles.statsButton} labelStyle={styles.statsButtonText}
+                  onPress={() => navigation.navigate('Journal')}
+                  title="Next"
+                  
+                />
+                </View>
+
+
+            </ScrollView>
+        
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#71B2E1',
+        
+    },
+    
+    titleText:{
+        fontSize: 30, 
+        textAlign: 'center',
+        fontWeight: 'bold' ,
+    
+    },
     textStyles: {
-        fontSize: 30
-    }
+        fontSize: 30,
+        padding: 10,
+
+    },
+    flexBox1: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        
+    },
+    flexBox2: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems:'center',        
+        
+    },
+    flexBox3: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent: 'center',
+        justifyContent: 'space-around',
+        padding: 10,
+        
+        
+        
+    },
+    
+    inputStyles: {
+        backgroundColor: 'white',
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5,
+        marginHorizontal: 15,
+        width: "92%"
+    },
+    backroundStyles: {
+        flex: 1,
+        justifyContent: 'center',
+        width: 160,
+        height: 150,       
+    },
+    buttonStyles: {
+        
+        width: 175,
+        height: 175,       
+    },
+    statsButton: {
+        height: 50,
+    },
+    statsButtonContent: {
+        height: 100,
+    }, 
 })
 
 export default EmotionDetailScreen;
