db5cee63369f913c5ab136a83eb5f6ac69211183
LongDangg <beckeu3@gmail.com>
2020-11-26 00:00:27 -0600

    Add TaskContext+MainTasksScreen

--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -1,13 +1,70 @@
 import { AsyncStorage } from 'react-native';
 import createCustomContext from './createCustomContext';
 
+let idCounter = 1;
+
 const tasksReducer = (state, action) => {
     switch (action.type) {
+        case 'get_tasks':
+            return action.payload;
         default: 
             return state;
     }
 }
 
 
+const getTasks = (dispatch) => async () => {
+    try {
+        const result = await AsyncStorage.getItem('TASKS');
+        if (result === null) {
+            dispatch({ type: 'get_tasks', payload: []})
+        } else {
+        dispatch({ type: 'get_tasks', payload: result});
+        console.log(result);
+        }
+    } catch {
+        console.log('error fetching tasks')
+    }
 
-export const { Provider, Context } = createCustomContext(tasksReducer, {}, []);
+}
+
+const deleteTasks = (dispatch) => async (id) => {
+    //delete tasks from async storage
+    //dispatch to reducer with payload id
+}
+
+const editTasks = (dispatch) => async (id, tasksName, callback) => {
+    //edit tasks in async storage
+    //dispatch to reducer with payload id, tasksname, daysofweek
+    //if callback to navigate back, call callback funct
+}
+
+const addTasks = (dispatch) => async (tasksName) => {
+    await AsyncStorage.removeItem('TASKS');
+    const tasksData = { id: idCounter, tasksName: tasksName, dateAdded: new Date(), checked: false, dates: [], date: ''};
+    idCounter += 1;
+    let tasksArray = [];
+    try {
+        let storedTasks = await AsyncStorage.getItem('TASKS');
+        if (storedTasks !== null) {
+            tasksArray = JSON.parse(storedTasks);
+        }
+        await AsyncStorage.removeItem('TASKS');
+        tasksArray.push(tasksData);
+        await AsyncStorage.setItem('TASKS', JSON.stringify(tasksArray));
+        const result = await AsyncStorage.getItem('TASKS');
+        console.log(result);
+    } catch (err) {
+
+    }
+    //add to async storage
+    //dispatch to reducer with payload tasksname, daysofweek, empty arr for dates, and generate an id (how?)
+    //if callback, callback()
+}
+
+const markTasks = (dispatch) => async (tasksName, date) => {
+    //edit tasks in async storage to update date
+    //dispatch to reducer dates, tasksname, id, daysofweek
+}
+
+export const { Provider, Context } = createCustomContext(tasksReducer, { getTasks, addTasks }, []);
--- src/screens/tasks/MainTasksScreen.js
+++ src/screens/tasks/MainTasksScreen.js
@@ -1,20 +1,194 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { View, SafeAreaView, ScrollView , Text, StyleSheet, TextInput, Pressable  } from 'react-native';
+import { Context } from '../../context/TasksContext';
+import { Card, List, Checkbox, Button, FAB, Icon,  } from 'react-native-paper';
+
+let tasks = [
+    {
+        checked: false,
+        name: 'Finish math homework',
+        date: 'Today \n at 12:00 pm'
+    },
+    {
+        checked: true,
+        name: 'Pick up medicine',
+        date: 'Tommorow \n at 10:00 am'
+    },
+    {
+        checked: true,
+        name: 'Study physics',
+        date: '10/16/2020 \n at 7:30pm'
+    },
+    {
+        checked: false,
+        name: '',
+        date:''
+    },
+    {
+        checked: false,
+        name: '',
+        date: ''
+    },
+]
+
+let inspirationalInsight = "You completed 15 tasks this week! Great job!";
+
+const TasksCheckbox = (prop) => {
+    const [checked, setChecked] = React.useState(prop.checked);
+  
+    return (
+        <Checkbox
+            status={checked ? 'checked' : 'unchecked'}
+            onPress={() => {
+                setChecked(!checked);
+            }}
+        />
+    );
+};
 
 const MainTasksScreen = ({ navigation }) => {
+    const { state, getTasks, addTasks } = useContext(Context);
+    
+    useEffect(() => {
+        getTasks();
+        const listener = navigation.addListener('didFocus', () => {
+            getTasks();
+        });
+
+        return () => {
+            listener.remove();
+        }
+    }, [])
+
     return (
-        <View>
-            <Text style={styles.textStyles}>Main Tasks Screen!</Text>
-            <Button title="Go to edit tasks screen" onPress={() => navigation.navigate('EditTasks')}/>
-            <Button title="Go to tasks stats screen" onPress={() => navigation.navigate('TasksStats')}/>
-        </View>
+        
+        <SafeAreaView style={styles.container}>
+        <FAB icon="plus" style={styles.addButton} onPress={() => addTasks("Make bed")} />    
+            <ScrollView style={styles.scrollView}>
+                <View style={styles.titleContainer}>
+                
+                <Text style={styles.taskTitle}>
+                    Your Tasks
+                </Text>
+
+                </View>
+                
+                    { 
+                        tasks.map((item, key) => (
+                            <Card style={styles.taskCard} key={key}>
+                                <View style={styles.taskItem}>
+                                    <TasksCheckbox style={styles.taskCheckbox} checked={item.checked} key={key} />
+                                    {(item.name ? 
+                                    <Text style={styles.taskName}>{item.name}</Text>:
+                                    <TextInput
+                                        placeholder="New tasks"
+                                        style={styles.tasksInput}
+                                    />)}
+                                    <View style={styles.taskDate}>
+                                        <Text>{item.date}</Text>
+                                    </View>
+
+                                </View>
+                            </Card>
+                        ))
+                    }
+                    <Text style={styles.inspirationalText}>{inspirationalInsight}</Text>  
+                    <Button icon="chart-bar" contentStyle={styles.statsButtonContent} style={styles.statsButton} labelStyle={styles.statsButtonText} mode="contained" onPress={() => console.log('Stats Pressed')}>
+                        See stats about your tasks
+                    </Button>  
+            </ScrollView>
+      </SafeAreaView>
     )
+
 }
 
 const styles = StyleSheet.create({
-    textStyles: {
-        fontSize: 30
-    }
-});
+    container: {
+        flex: 1,
+    },
+    scrollView: {
+        flex: 1,
+        marginTop:8,
+    },
+    buttonStyles: {
+        margin: 15
+    },
+    titleContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        marginHorizontal: 16,
+        marginVertical: 2,
+    },
+    taskTitle: {
+        flexGrow: 1,
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    addButton: {
+        margin: 16,
+        padding: 8,
+        position:'absolute',
+        right:16,
+        top:-56,
+        borderRadius: 100
+        
+
+    },
+
+    taskCard: {
+        marginTop: 10,
+        marginHorizontal: 16,
+
+    },
+
+    taskItem: {
+
+        flex: 1,
+        flexDirection: 'row',
+        padding: 10,
+        alignItems:"center",
+        
+    },
+
+    taskCheckbox: {
+        flexGrow: 0, 
+    },
+    taskInput: {
+        flexGrow: 1, 
+        borderBottomColor: '#e3e3e3',
+        borderBottomWidth: 1,
+        paddingHorizontal: 5,
+        fontSize: 18,
+        width: 250,
+        backgroundColor: '#F0F0F0'
+    },
+    taskName: {
+        flexGrow: 1, 
+        fontSize: 15,
+        width: 180,
+        lineHeight: 36,
+
+    },
+    taskDate: {
+        flexGrow: 0, 
+        marginLeft: 5,
+        fontSize: 8,
+    },
+    inspirationalText: {
+        fontSize:36,
+        textAlign: 'center',
+        paddingVertical: 24,
+        paddingHorizontal: 16,
+        opacity: .4,
+
+    },
+    statsButton: {
+        marginVertical: 30,
+        marginHorizontal: 50,
+    },
+    statsButtonContent: {
+        height: 100,
+    },    
+})
 
 export default MainTasksScreen;
