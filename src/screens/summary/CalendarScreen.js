f516f2ffe6e2f437db71e650e171fb58704ea969
Scott Howard <github@scottah.com>
2020-12-10 02:59:14 -0600

    Dark mode all the things!

--- src/screens/summary/CalendarScreen.js
+++ src/screens/summary/CalendarScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import {Colors} from '../../components/DarkTheme';
 
 const CalendarScreen = ({ navigation }) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.textStyles}>Calendar Screen!</Text>
             <Button title="Go to Day Detail Screen" onPress={() => navigation.navigate('DayDetail')}/>
             <Button title="Go to Main Stats Screen" onPress={() => navigation.navigate("MainStats")} />
@@ -12,8 +13,13 @@ const CalendarScreen = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.background,
+        height: '100%'
+    },
     textStyles: {
-        fontSize: 30
+        fontSize: 30,
+        color: Colors.text.primary
     }
 })
 
